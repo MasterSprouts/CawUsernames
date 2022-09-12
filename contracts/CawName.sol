01cde95ebded60ba07280549e14d61dd77df603a
Master Sprouts <email@email.email>
2022-09-13 00:24:13 +0300

    Great progress on the contracts - added deposit/withdrawl, caw, recaw, like, and follow. More to do here, once the external DB piece is worked out.

--- contracts/CawName.sol
+++ contracts/CawName.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 
 import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../node_modules/@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 import "./CawNameURI.sol";
 
@@ -18,9 +19,19 @@ contract CawName is
   CawNameURI public uriGenerator;
 
   string[] public usernames;
-  mapping(string => uint256) idsByUsername;
+  mapping(uint256 => uint256) public actions;
+  mapping(string => uint256) public idByUsername;
+  mapping(uint256 => uint256) public cawBalanceOf;
+  mapping(uint256 => uint256) public previousOwners;
+
+  // tokenId => actionNumber => timestamp ?????????????
+  mapping(uint256 => mapping(uint256 => uint256)) public actionTimestamps;
+
+  uint256 public stakePool;
+  bytes32 public eip712DomainHash;
 
   constructor(address _gui) ERC721("CAW NAME", "cawNAME") {
+    eip712DomainHash = generateDomainHash();
     uriGenerator = CawNameURI(_gui);
   }
 
@@ -32,8 +43,22 @@ contract CawName is
     uriGenerator = CawNameURI(_gui);
   }
 
+  // This might be needed to validate CAWs on the
+  // front end after a token has been transfered
+  //
+  // function _afterTokenTransfer(
+  //   address from,
+  //   address to,
+  //   uint256 tokenId
+  // ) internal virtual override {
+  //   super._afterTokenTransfer(from, to, tokenId);
+  //
+  //   if (from != address(0))
+  //     previousOwners[tokenId].add(from);
+  // }
+
   function mint(string memory username) public {
-    require(idsByUsername[username] == 0, "Username has already been taken");
+    require(idByUsername[username] == 0, "Username has already been taken");
     require(isValidUsername(username), "Username must only consist of 1-255 lowercase letters and numbers");
     uint256 amount = costOfName(username);
 
@@ -43,7 +68,7 @@ contract CawName is
 
     usernames.push(username);
     uint256 newId = usernames.length;
-    idsByUsername[username] = newId;
+    idByUsername[username] = newId;
 
     _safeMint(_msgSender(), newId);
   }
@@ -112,6 +137,128 @@ contract CawName is
     }
 
     return true;
+  }
+
+  function deposit(uint256 tokenId, uint256 amount) public {
+    require(ownerOf(tokenId) == msg.sender, "can not deposit into a CawName that you do not own");
+
+    CAW.transferFrom(msg.sender, address(this), amount);
+    cawBalanceOf[tokenId] += amount;
+  }
+
+  function withdraw(uint256 tokenId, uint256 amount) public {
+    require(ownerOf(tokenId) == msg.sender, "can not withdraw from a CawName that you do not own");
+    require(cawBalanceOf[tokenId] >= amount, "withdraw amount is greater than this CAW balance");
+
+    CAW.transfer(msg.sender, amount);
+    cawBalanceOf[tokenId] -= amount;
+  }
+
+  function caw(
+    uint8 v, bytes32 r, bytes32 s,
+    uint256 action,
+    uint256 tokenId,
+    uint256 timestamp,
+    string memory text
+  ) external {
+    bytes memory functionCall = abi.encode(
+      keccak256("Caw(uint256 action,uint256 tokenId,uint256 timestamp,string text)"), action, tokenId, timestamp, text
+    );
+    address signer = getSigner(functionCall, v, r, s);
+    require(signer == ownerOf(tokenId), "signer is not owner of this CawName");
+    require(cawBalanceOf[tokenId] >= 5000, 'you need at least 5000 CAW to post a caw');
+
+    actionTimestamps[tokenId][action] = timestamp;
+    cawBalanceOf[tokenId] -= 5000;
+    actions[tokenId] += 1;
+    stakePool += 5000;
+  }
+
+  function likeCaw(
+    uint8 v, bytes32 r, bytes32 s,
+    uint256 senderTokenId,
+    uint256 posterTokenId,
+    uint256 cawId
+  ) external {
+    bytes memory functionCall = abi.encode(
+      keccak256("Caw(uint256 cawId,uint256 senderTrokenId)"), posterTokenId, senderTokenId, cawId
+    );
+    address signer = getSigner(functionCall, v, r, s);
+    require(signer == ownerOf(senderTokenId), "signer is not owner of this CawName");
+    require(cawBalanceOf[senderTokenId] >= 2000, 'you need at least 2000 CAW to like a caw');
+
+    cawBalanceOf[senderTokenId] -= 2000;
+    cawBalanceOf[posterTokenId] += 1600;
+    actions[senderTokenId] += 1;
+    stakePool += 400;
+  }
+
+  function reCaw(
+    uint8 v, bytes32 r, bytes32 s,
+    uint256 posterTokenId,
+    uint256 senderTokenId,
+    uint256 cawId
+  ) external {
+    bytes memory functionCall = abi.encode(
+      keccak256("Caw(uint256 posterTokenId,uint256 senderTokenId)"), posterTokenId, senderTokenId, cawId
+    );
+    address signer = getSigner(functionCall, v, r, s);
+    require(signer == ownerOf(senderTokenId), "signer is not owner of this CawName");
+    require(cawBalanceOf[senderTokenId] >= 4000, 'you need at least 4000 CAW to re-caw');
+
+    cawBalanceOf[senderTokenId] -= 4000;
+    cawBalanceOf[posterTokenId] += 2000;
+    actions[senderTokenId] += 1;
+    stakePool += 2000;
+  }
+
+  function follow(
+    uint8 v, bytes32 r, bytes32 s,
+    uint256 followerTokenId,
+    uint256 followeeTokenId
+  ) external {
+    bytes memory functionCall = abi.encode(
+      keccak256("Caw(uint256 followerTokenId,uint256 followeeTokenId)"), followeeTokenId, followerTokenId
+    );
+    address signer = getSigner(functionCall, v, r, s);
+    require(signer == ownerOf(followerTokenId), "signer is not owner of this CawName");
+    require(cawBalanceOf[followerTokenId] >= 30000, 'you need at least 30000 CAW to follow a user');
+
+    cawBalanceOf[followerTokenId] -= 30000;
+    cawBalanceOf[followerTokenId] += 24000;
+    actions[followerTokenId] += 1;
+    stakePool += 6000;
+  }
+
+  function getSigner(
+    bytes memory functionCall,
+    uint8 v, bytes32 r, bytes32 s
+  ) public view returns (address) {
+    uint256 chainId;
+    assembly {
+      chainId := chainid()
+    }
+
+    bytes32 hash = keccak256(abi.encodePacked("\x19\x01", eip712DomainHash, keccak256(functionCall)));
+    return ecrecover(hash, v,r,s);
+  }
+
+  function generateDomainHash() internal view returns (bytes32) {
+    uint256 chainId;
+    assembly {
+      chainId := chainid()
+    }
+    return keccak256(
+      abi.encode(
+        keccak256(
+          "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
+        ),
+        keccak256(bytes("CawNet")),
+        keccak256(bytes("1")),
+        chainId,
+        address(this)
+      )
+    );
   }
 
 }
