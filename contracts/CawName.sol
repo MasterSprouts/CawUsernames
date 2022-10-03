c783e9085485d863984fcdac1cc75dc5c899488a
Master Sprouts <email@email.email>
2022-10-03 16:47:16 +0200

    Fixing file title copied from another project on etherscan

--- contracts/CawName.sol
+++ contracts/CawName.sol
@@ -1,4 +1,4 @@
-// contracts/ChurchEggs.sol
+// contracts/CawName.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
--- contracts/CawNameURI.sol
+++ contracts/CawNameURI.sol
@@ -1,4 +1,4 @@
-// contracts/ChurchEggs.sol
+// contracts/CawNameURI.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
